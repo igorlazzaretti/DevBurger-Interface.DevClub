bf5e0f4810b71113d34b658bbf9f636ba5c86206
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-12-26 21:48:40 -0300

    :man_technologist:Interface DevBurguer update: finalizada tela de Login. A11P1

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,4 +1,6 @@
+// Imagens
 import Logo from "../../assets/logo.svg";
+
 import {
     Container,
     Form,
@@ -7,18 +9,74 @@ import {
     RigthContainer,
     Title,
     Link,
-
 } from "./styles";
 
 // Components: Button
 import { Button } from '../../components/Button/index.jsx';
 
+// Hook Form e Yup
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 export function Login() {
+
+    // Validação Yup
+    const schema = yup.object({
+        email: yup.string()
+            .email('Digite um e-mail válido')
+            .required('O e-mail é obrigatório'),
+        password: yup.string()
+            .min(6, 'A senha deve ter pelo menos 6 caracteres')
+            .required('Digite a senha'),
+    })
+        .required();
+
+    // Hook Form
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    });
+    console.log(errors)
+    const onSubmit = async (data) => {
+        console.log(data)
+        const { data: userData } = await toast.promise(
+            api.post('/session', {
+                email: data.email,
+                password: data.password,
+            }),
+            {
+                pending: 'Verificando dados',
+                success: {
+                    render() {
+                        setTimeout(() => {
+                            if (userData.admin) {
+                                navigate('/pedidos')
+                            } else {
+                                navigate('/')
+                            }
+                        }, 2000);
+                        return 'Seja bem-vindo'
+                    },
+                },
+                error: 'Dados incorretos',
+            },
+        );
+        putUserData(userData);
+
+
+    };
+
+
+
+    // Retorno
     return (
         <Container>
             <LeftContainer>
-                <img src={Logo} alt="Logo" />
+                <img src={Logo} alt="logo-devburger" />
             </LeftContainer>
             <RigthContainer>
                 <Title>
@@ -26,21 +84,23 @@ export function Login() {
                     <br />
                     Acesse com seu <span>Login e senha</span>
                 </Title>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <InputContainer>
                         <label>Email</label>
-                        <input type="email" />
+                        <input type="email" {...register("email")} />
+                        <p>{errors?.email?.message}</p>
                     </InputContainer>
 
                     <InputContainer>
                         <label>Senha</label>
-                        <input type="password" />
+                        <input type="password" {...register("password")} />
+                        <p>{errors?.password?.message}</p>
                     </InputContainer>
-                    <Button type="submit">Entrar</Button>
+                    <Button type="submit" >Entrar</Button>
                 </Form>
 
                 <p>
-                    Não possui conta? <Link>Clique aqui</Link>
+                    Não possui conta? <Link to="/cadastro">Clique aqui</Link>
                 </p>
             </RigthContainer>
         </Container>
