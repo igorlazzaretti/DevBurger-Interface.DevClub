b29a7cfd9e4cc2ede3ddbcd77be445ed708277b9
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2025-08-19 04:32:13 -0300

    :man_technologist:Interface DevBurguer Módulo 1 Aula 24 - Finalizando o Carrosel

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Container, Title, ContainerItems } from "./styles";
 
 export function CategoriesCarousel() {
   const [categories, setCategories] = useState([]);
@@ -15,9 +18,43 @@ export function CategoriesCarousel() {
     }
     loadCategories();
   }, []);
+
+  // Responsividade do Carrossel exigido pela biblioteca react-multi-carousel
+  const responsive = {
+    superLargeDesktop: {
+        breakpoint: {max: 4000, min: 3000},
+        items: 4,
+    },
+    desktop: {
+        breakpoint: {max: 3000, min: 1280},
+        items: 4,
+    },
+    tablet: {
+        breakpoint: {max: 1280, min: 690},
+        items: 3,
+    },
+    mobile: {
+        breakpoint: {max: 690, min: 0},
+        items: 2,
+    },
+  };
+
+
   return (
-    <div>
-      <h1>Carrossel de Categorias</h1>
-    </div>
+    <Container>
+      <Title>Categorias</Title>
+      <Carousel
+          responsive={responsive}
+          infinite={true}
+          partialVisible={false}
+          itemClass="carousel-item"
+      >
+        {categories.map(({ id, url, name }) => (
+          <ContainerItems key={id} $imageurl={url} >
+            <p>{name}</p>
+          </ContainerItems>
+        ))}
+      </Carousel>
+    </Container>
   )
 }
--- src/components/CategoriesCarousel/styles.js
+++ src/components/CategoriesCarousel/styles.js
@@ -1,2 +1,50 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  .carousel-item {
+    padding-right: 40px;
+    padding-left: 40px;
+  }
+`;
+
+export const Title = styled.h2`
+    font-size: 32px;
+    font-weight: 800;
+    color: #9758a6;
+    padding-bottom: 12px;
+    position: relative;
+    text-align: center;
+    margin-bottom: 40px;
+    margin-top: 20px;
+
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: calc(50% - 28px);
+      width: 56px;
+      height: 4px;
+      background: #9758a6;
+    }
+`;
+
+export const ContainerItems = styled.div`
+  background: url(${props => props.$imageurl}) no-repeat center center;
+  display: flex;
+  align-items: center;
+  padding: 20px 10px;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  border-radius: 20px;
+
+  p {
+    color: #fff;
+    background-color: rgba(0,0,0,0.5);
+    padding: 10px 30px;
+    border-radius: 30px;
+    font-size: 22.5px;
+    font-weight: bold;
+    margin-top: 50px;
+  }
+`;
